5f2d6a4a879d28ed6c44488701cf887b9a8e2175
Ngo Van Cuong <cuongnv@lumi.vn>
2022-07-13 15:32:05 +0700

    Update CleanCode

--- app/network/network.c
+++ app/network/network.c
@@ -32,15 +32,15 @@
 /******************************************************************************/
 /*                            EVENTS CONTROL                            	  */
 /******************************************************************************/
-EmberEventControl joinNetworkEventControl;
+EmberEventControl JoinNetworkEventControl;
 /******************************************************************************/
 /*                              PRIVATE DATA                                  */
 /******************************************************************************/
 /******************************************************************************/
 /*                              PRIVATE DATA                                  */
 /******************************************************************************/
-u32_t bytimeFindAndJoin = 0;
-networkEventHandler networkEventHandle = NULL;
+u32_t g_bytimeFindAndJoin = 0;
+networkEventHandler g_networkEvent = NULL;
 /******************************************************************************/
 /*                              EXPORTED DATA                                 */
 /******************************************************************************/
@@ -48,7 +48,6 @@ networkEventHandler networkEventHandle = NULL;
 /******************************************************************************/
 /*                            PRIVATE FUNCTIONS                               */
 /******************************************************************************/
-static void joinNetworkEventHandler(void);
 static void networkStopFindAndJoin(void);
 /******************************************************************************/
 /*
@@ -56,13 +55,13 @@ static void networkStopFindAndJoin(void);
  *
  * @brief				: Handle event network.
  *
- * @parameter			: networkEventHandler
+ * @parameter			: g_networkEventHandler
  *
  * @return value		: None
  */
 void networkInit(networkEventHandler networkResult)
 {
-	networkEventHandle = networkResult;
+	g_networkEvent = networkResult;
 }
 
 /*
@@ -78,7 +77,7 @@ void networkFindAndJoin(void)
 {
 	if(emberAfNetworkState() == EMBER_NO_NETWORK)
 	{
-		emberEventControlSetDelayMS(joinNetworkEventControl, 2000);
+		emberEventControlSetDelayMS(JoinNetworkEventControl, 2000);
 	}
 }
 
@@ -99,7 +98,7 @@ static void networkStopFindAndJoin(void)
 
 
 /*
- * @function 			: joinNetworkEventHandler
+ * @function 			: joing_networkEventHandler
  *
  * @brief				: Handle Event Join network
  *
@@ -107,14 +106,14 @@ static void networkStopFindAndJoin(void)
  *
  * @return value		: None
  */
-static void joinNetworkEventHandler(void)
+void JoinNetworkEventHandler(void)
 {
-	emberEventControlSetInactive(joinNetworkEventControl);
+	emberEventControlSetInactive(JoinNetworkEventControl);
 
 	if(emberAfNetworkState() == EMBER_NO_NETWORK)
 	{
 		emberAfPluginNetworkSteeringStart();
-		bytimeFindAndJoin++;
+		g_bytimeFindAndJoin++;
 	}
 }
 
@@ -134,19 +133,19 @@ bool emberAfStackStatusCallback(EmberStatus networkStatus)
 
 	if(networkStatus == EMBER_NETWORK_UP)
 	{
-		if(bytimeFindAndJoin>0)// vao mang thanh cong
+		if(g_bytimeFindAndJoin>0)// vao mang thanh cong
 		{
 			networkStopFindAndJoin();
-			if(networkEventHandle != NULL)
+			if(g_networkEvent != NULL)
 			{
-				networkEventHandle(NETWORK_JOIN_SUCCESS);
+			    g_networkEvent(NETWORK_JOIN_SUCCESS);
 				emberAfCorePrintln("NETWORK_JOIN_SUCCESS");
 			}
 		}else
 		{
-			if(networkEventHandle != NULL)
+			if(g_networkEvent != NULL)
 			{
-				networkEventHandle(NETWORK_HAS_PARENT);
+			    g_networkEvent(NETWORK_HAS_PARENT);
 				emberAfCorePrintln("NETWORK_HAS_PARENT");
 			}
 		}
@@ -157,21 +156,21 @@ bool emberAfStackStatusCallback(EmberStatus networkStatus)
 		EmberNetworkStatus nwkStatusCurrent = emberAfNetworkState();
 		if(nwkStatusCurrent == EMBER_NO_NETWORK)
 		{
-			if(networkEventHandle != NULL)
+			if(g_networkEvent != NULL)
 			{
-				networkEventHandle(NETWORK_OUT_NETWORK);
+			    g_networkEvent(NETWORK_OUT_NETWORK);
 				emberAfCorePrintln("NETWORK_OUT_NETWORK");
 			}
 		}
 		else if(nwkStatusCurrent == EMBER_JOINED_NETWORK_NO_PARENT){
 			emberAfCorePrintln("NETWORK_LOST_PARENT");
-			networkEventHandle(NETWORK_LOST_PARENT);
+			g_networkEvent(NETWORK_LOST_PARENT);
 		}
 	}
 	if(networkStatus == EMBER_JOIN_FAILED)
 	{
 		emberAfCorePrintln("NETWORK_JOIN_FAIL");
-		networkEventHandle(NETWORK_JOIN_FAIL);
+		g_networkEvent(NETWORK_JOIN_FAIL);
 	}
 	return FALSE;
 }
